eb44662f216e7412db838970c53f50527e317bd1
Ulfur <ulfsen@gmail.com>
2017-03-23 15:47:25 +0000

    bla

--- app/src/main/java/com/example/ulfurae/ble1/AddExerciseActivity.java
+++ app/src/main/java/com/example/ulfurae/ble1/AddExerciseActivity.java
@@ -1,6 +1,5 @@
 package com.example.ulfurae.ble1;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,20 +11,9 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONStringer;
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-import entities.Exercise;
 
 /**
  * Created by heidrunh on 2.3.2017.
@@ -35,42 +23,16 @@ public class AddExerciseActivity extends MenuActivity{
 
     private Spinner spinner;
     private static List<String> exercisesList = new ArrayList<String>();
+    private Bundle extras = getIntent().getExtras();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addexercise);
 
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
-
-        //get the list of exercises from database for the dropdown list
-        try {
-            String url = Uri.parse("http://10.0.2.2:8080/getExercises")
-                    .buildUpon()
-                    .build().toString();
-            Log.i("Urlið", url);
-            String jsonString = FetchData.getUrlString(url);
-            Log.i("FetchData","Received JSON: "+jsonString);
-            JSONArray jsonArray = new JSONArray(jsonString);
-            exercisesList = FetchData.parseExercise(exercisesList, jsonArray);
-
-        } catch(IOException ioe) {
-            Log.e("FetchData", "Failed to fetch items", ioe);
-        } catch (JSONException je) {
-            Log.e("FetchData","Failed to parse JSON", je);
-        } catch(ParseException pe) {
-            Log.e("FetchData", "Failed to parse date", pe);
-        }
-
-        String[] exercises = new String[exercisesList.size()];
-        exercises = exercisesList.toArray(exercises);
-
         //dropdown list of exercises
         spinner = (Spinner)findViewById(R.id.exerciseSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, exercises);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, extras.getStringArray("exercises"));
         spinner.setAdapter(adapter);
 
         exercisesList.clear();
@@ -91,7 +53,7 @@ public class AddExerciseActivity extends MenuActivity{
         String exercise = exerciseTxt.getSelectedItem().toString();
 
         //get logged in user
-        Bundle extras = getIntent().getExtras();
+
         String userName = extras.getString("Username");
 
         if (android.os.Build.VERSION.SDK_INT > 9) {
--- app/src/main/java/com/example/ulfurae/ble1/MainActivity.java
+++ app/src/main/java/com/example/ulfurae/ble1/MainActivity.java
@@ -1,18 +1,60 @@
 package com.example.ulfurae.ble1;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends MenuActivity {
 
     String loggedInUser = "tester2";
 
+    private static List<String> exercisesList = new ArrayList<String>();
+    private static String[] exercises;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        //get the list of exercises from database for the dropdown list
+        try {
+            String url = Uri.parse("http://10.0.2.2:8080/getExercises")
+                    .buildUpon()
+                    .build().toString();
+            Log.i("Urlið", url);
+            String jsonString = FetchData.getUrlString(url);
+            Log.i("FetchData","Received JSON: "+jsonString);
+            JSONArray jsonArray = new JSONArray(jsonString);
+            exercisesList = FetchData.parseExercise(exercisesList, jsonArray);
+
+        } catch(IOException ioe) {
+            Log.e("FetchData", "Failed to fetch items", ioe);
+        } catch (JSONException je) {
+            Log.e("FetchData","Failed to parse JSON", je);
+        } catch(ParseException pe) {
+            Log.e("FetchData", "Failed to parse date", pe);
+        }
+
+        exercises = new String[exercisesList.size()];
+        exercises = exercisesList.toArray(exercises);
+
     }
 
 
@@ -50,6 +92,7 @@ public class MainActivity extends MenuActivity {
     public void viewAddExercise(View view) {
         Intent intent = new Intent(this, AddExerciseActivity.class);
         intent.putExtra("Username",loggedInUser);
+        intent.putExtra("exercises",exercises);
         startActivity(intent);
     }
 
